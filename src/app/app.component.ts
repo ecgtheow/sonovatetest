9190b5dd2a2fda59fb1f8ccca52f2c1b8743a45a
Dick Porter <dick@ecgtheow.com>
2017-07-08 11:59:47 +0100

    Gather all the RXJS operator loading into one place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import './rxjs-operators';
+
 import { AlertsService } from './alerts.service';
 
 @Component({
---
+++ src/app/rxjs-operators.ts
@@ -0,0 +1,6 @@
+import 'rxjs/add/observable/of';
+
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/switchMap';
